681293d98373020eb86a2ea66438f798630f7bc5
james <james@irmkr>
2022-09-08 21:29:57 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require("./model/User");
+
+const config = require('./config/key');
+
+//서버에서 분석해서 가져올 수 있게
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://badagogi:1q2w3e4r@cluster0.tebnyyz.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     /*useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex : true,
@@ -11,10 +21,24 @@ mongoose.connect('mongodb+srv://badagogi:1q2w3e4r@cluster0.tebnyyz.mongodb.net/?
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log('err ', err))
 
-
 app.get('/', (req,res) =>
-    res.send('Hello world 안녕하세요~!      ')
+    res.send('Hello world')
 );
+
+app.post('/register', (req, res) => {
+    //회원가입할때 필요한 정보들을 클라이언트에서 가져오면
+    //그것들을 DB에 넣어준다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 
